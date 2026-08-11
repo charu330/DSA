0ef704d5d43be1cef383fd7ec9d71e29170b93b8
charu330 <41389124+charu330@users.noreply.github.com>
2026-08-11 23:45:55 +0530

    Create subarraySum.py

---
+++ Arrays/subarraySum.py
@@ -0,0 +1,25 @@
+# 560. Subarray Sum Equals K
+
+
+class Solution:
+    def subarraySum(self, nums: List[int], k: int) -> int:
+  
+        count={}
+        csum=0
+        res=0
+
+        for n in nums:
+            
+            csum += n
+            if csum==k:   
+                res+=1
+            if csum-k in count: 
+                res+=count[csum-k]
+            
+            if csum in count:
+                count[csum]+=1
+            else:
+                count[csum]=1
+
+        return res
+        
